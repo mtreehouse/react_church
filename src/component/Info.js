8f5770531090b9cdfb96785fffc6f6e32173aa23
Yunwoo Kim <sym3927@naver.com>
2022-02-05 01:00:19 +0900

    working on spreadsheet

--- src/component/Info.js
+++ src/component/Info.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 export default function Info() {
+    const doc = new GoogleSpreadsheet('1r8mvSOMi8tZe8D6-6SocafJ9_LeURIPVphH71a7aONE');
+    doc.useServiceAccountAuth({
+      client_email: process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
+    });
+    const sheet = function(){
+        console.log(process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL);
+    }
 
     return (
-        <div>hi</div>
+        <div>
+            <Button 
+            onClick={sheet}
+            >
+            test
+            </Button>
+        </div>
     );
 }
