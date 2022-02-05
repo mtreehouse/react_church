eb782650da14a37dccce61fc5ac2b7c5dfa4ad58
Yunwoo Kim <sym3927@naver.com>
2022-02-05 12:43:22 +0900

    error spreadsheet

--- src/component/Info.js
+++ src/component/Info.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Button from '@mui/material/Button';
-import { GoogleSpreadsheet } from 'google-spreadsheet'
+// import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 export default function Info() {
-    const doc = new GoogleSpreadsheet('1r8mvSOMi8tZe8D6-6SocafJ9_LeURIPVphH71a7aONE');
-    doc.useServiceAccountAuth({
-      client_email: process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
-    });
+    // const doc = new GoogleSpreadsheet('1r8mvSOMi8tZe8D6-6SocafJ9_LeURIPVphH71a7aONE');
+    // doc.useServiceAccountAuth({
+    //   client_email: process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    //   private_key: process.env.REACT_APP_GOOGLE_PRIVATE_KEY,
+    // });
+    const {GoogleSpreadsheet} = require("google-spreadsheet");
+
     const sheet = function(){
         console.log(process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL);
     }
